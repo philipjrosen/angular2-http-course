97cfcac40d5a46bb1542a6883b3ee4e26f6d75d6
Philip Rosen <philipjrosen@gmail.com>
2017-02-13 11:07:00 -0800

    Starting User Service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { User } from './shared/models/user';
+import { UserService } from './shared/services/user.service';
 
 @Component({
   selector: 'my-app',
@@ -20,13 +21,12 @@ import { User } from './shared/models/user';
 export class AppComponent implements OnInit {
   users: User[];
 
-  constructor(private http: Http) {
+  constructor(private service: UserService) {
 
   }
 
   ngOnInit() {
-    this.http.get('https://reqres.in/api/users')
-      .map(res => res.json().data)
+    this.service.getUsers()
       .subscribe(users => this.users = users);
   }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,11 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
+import { UserService } from './shared/services/user.service';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
@@ -15,6 +18,9 @@ import 'rxjs/add/operator/toPromise';
   ],
   declarations: [ 
     AppComponent 
+  ],
+  providers: [
+    UserService
   ],
   bootstrap: [ AppComponent ]
 })
