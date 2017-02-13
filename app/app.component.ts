d31f467cbeeba4c10d338d00dbca701ce1ffacaa
Philip Rosen <philipjrosen@gmail.com>
2017-02-13 10:35:14 -0800

    RxJS Operators

--- app/app.component.ts
+++ app/app.component.ts
@@ -26,9 +26,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.http.get('https://reqres.in/api/users')
-      .subscribe(data => {
-        console.log(data.json());
-        this.users = data.json().data;
-      });
+      .map(res => res.json().data)
+      .subscribe(users => this.users = users);
   }
 }
